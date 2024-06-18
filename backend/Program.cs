a3009bc1088d58bdb7dabe561a0f843b2871139e
raghpai13 <raghavendra.pai.13@gmail.com>
2024-06-18 15:38:44 +0530

    "Updated"

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,12 +1,17 @@
+using DotNetEnv;
 using ExpenseTracker.Data;
 using ExpenseTracker.Repository.Implementation;
 using ExpenseTracker.Repository.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+Env.Load();
+//var ClientId = Environment.GetEnvironmentVariable("GoogleOAuth_ClientID");
+//var ClientSecret= Environment.GetEnvironmentVariable("GoogleOAuth_ClientSecret");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,7 +27,20 @@ builder.Services.AddCors(options =>
                .AllowAnyHeader();
     });
 });
-
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = Environment.GetEnvironmentVariable("GoogleOAuth_ClientID");
+    options.ClientSecret = Environment.GetEnvironmentVariable("GoogleOAuth_ClientSecret");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+});
 //Added Db Context
 builder.Services.AddDbContext<ExpenseTrackerDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ExpenseTrackerCon")));
