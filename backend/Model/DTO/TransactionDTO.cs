5e6e5798e8c5f8108fd2af6da489faa4465d0113
raghpai13 <raghavendra.pai.13@gmail.com>
2024-07-19 13:05:05 +0530

    Transaction final push

--- backend/Model/DTO/TransactionDTO.cs
+++ backend/Model/DTO/TransactionDTO.cs
@@ -11,6 +11,7 @@ namespace ExpenseTracker.Model.DTO
         public float NewBalance { get; set; }
         public string Remarks { get; set; }
         public string AccountNo { get; set; }
+        public string BankName { get; set; }
         public string EmailId { get; set; }
         public string Type { get; set; } // "Income" or "Expense"
         public int? CategoryId { get; set; } // Nullable to accommodate income transactions
