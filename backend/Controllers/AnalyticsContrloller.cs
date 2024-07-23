527ed8c789fd0a6640afed0dd1a7812799581331
raghpai13 <raghavendra.pai.13@gmail.com>
2024-07-23 12:40:14 +0530

    Analytics Bar Chart completed

--- backend/Controllers/AnalyticsContrloller.cs
+++ backend/Controllers/AnalyticsContrloller.cs
@@ -1,8 +1,10 @@
 ﻿using ExpenseTracker.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace ExpenseTracker.Api.Controllers
+namespace ExpenseTracker.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -15,86 +17,87 @@ namespace ExpenseTracker.Api.Controllers
             _analyticsRepository = analyticsRepository;
         }
 
-        [HttpGet("expenses/today")]
-        public async Task<IActionResult> GetTotalExpenseToday(string email)
+        [HttpGet("total-expense-today")]
+        public async Task<IActionResult> GetTotalExpenseToday([FromQuery] string email, [FromQuery] int date, [FromQuery] int month, [FromQuery] int year)
         {
-            var totalExpense = await _analyticsRepository.GetTotalExpenseTodayAsync(email);
-            return Ok(totalExpense);
-        }
-
-        [HttpGet("incomes/today")]
-        public async Task<IActionResult> GetTotalIncomeToday(string email)
-        {
-            var totalIncome = await _analyticsRepository.GetTotalIncomeTodayAsync(email);
-            return Ok(totalIncome);
-        }
-
-        [HttpGet("expenses/week")]
-        public async Task<IActionResult> GetTotalExpenseThisWeek(string email)
-        {
-            var totalExpense = await _analyticsRepository.GetTotalExpenseThisWeekAsync(email);
-            return Ok(totalExpense);
-        }
-
-        [HttpGet("incomes/week")]
-        public async Task<IActionResult> GetTotalIncomeThisWeek(string email)
-        {
-            var totalIncome = await _analyticsRepository.GetTotalIncomeThisWeekAsync(email);
-            return Ok(totalIncome);
-        }
-
-        [HttpGet("expenses/month")]
-        public async Task<IActionResult> GetTotalExpenseByMonth(string email, int year, int month)
-        {
-            var totalExpense = await _analyticsRepository.GetTotalExpenseByMonthAsync(email, year, month);
-            return Ok(totalExpense);
-        }
-
-        [HttpGet("incomes/month")]
-        public async Task<IActionResult> GetTotalIncomeByMonth(string email, int year, int month)
-        {
-            var totalIncome = await _analyticsRepository.GetTotalIncomeByMonthAsync(email, year, month);
-            return Ok(totalIncome);
-        }
-
-        [HttpGet("expenses/year")]
-        public async Task<IActionResult> GetTotalExpenseThisYear(string email)
-        {
-            var totalExpense = await _analyticsRepository.GetTotalExpenseThisYearAsync(email);
-            return Ok(totalExpense);
-        }
-
-        [HttpGet("incomes/year")]
-        public async Task<IActionResult> GetTotalIncomeThisYear(string email)
-        {
-            var totalIncome = await _analyticsRepository.GetTotalIncomeThisYearAsync(email);
-            return Ok(totalIncome);
-        }
-        [HttpGet("expenses/today/by-category")]
-        public async Task<IActionResult> GetTotalExpenseByCategoryToday(string email)
-        {
-            var result = await _analyticsRepository.GetTotalExpenseByCategoryTodayAsync(email);
+            var result = await _analyticsRepository.GetTotalExpenseTodayAsync(email, date, month, year);
             return Ok(result);
         }
 
-        [HttpGet("expenses/week/by-category")]
-        public async Task<IActionResult> GetTotalExpenseByCategoryThisWeek(string email)
+        [HttpGet("total-income-today")]
+        public async Task<IActionResult> GetTotalIncomeToday([FromQuery] string email, [FromQuery] int date, [FromQuery] int month, [FromQuery] int year)
         {
-            var result = await _analyticsRepository.GetTotalExpenseByCategoryThisWeekAsync(email);
+            var result = await _analyticsRepository.GetTotalIncomeTodayAsync(email, date, month, year);
             return Ok(result);
         }
 
-        [HttpGet("expenses/month/by-category")]
-        public async Task<IActionResult> GetTotalExpenseByCategoryThisMonth(string email)
+        [HttpGet("total-expense-this-week")]
+        public async Task<IActionResult> GetTotalExpenseThisWeek([FromQuery] string email, [FromQuery] int date, [FromQuery] int month, [FromQuery] int year)
         {
-            var result = await _analyticsRepository.GetTotalExpenseByCategoryThisMonthAsync(email);
+            var result = await _analyticsRepository.GetTotalExpenseThisWeekAsync(email, date, month, year);
             return Ok(result);
         }
 
-        [HttpGet("expenses/year/by-category")]
-        public async Task<IActionResult> GetTotalExpenseByCategoryThisYear(string email)
+        [HttpGet("total-income-this-week")]
+        public async Task<IActionResult> GetTotalIncomeThisWeek([FromQuery] string email, [FromQuery] int date, [FromQuery] int month, [FromQuery] int year)
         {
-            var result = await _analyticsRepository.GetTotalExpenseByCategoryThisYearAsync(email);
+            var result = await _analyticsRepository.GetTotalIncomeThisWeekAsync(email, date, month, year);
+            return Ok(result);
+        }
+
+        [HttpGet("total-expense-by-month")]
+        public async Task<IActionResult> GetTotalExpenseByMonth([FromQuery] string email, [FromQuery] int year, [FromQuery] int month)
+        {
+            var result = await _analyticsRepository.GetTotalExpenseByMonthAsync(email, year, month);
+            return Ok(result);
+        }
+
+        [HttpGet("total-income-by-month")]
+        public async Task<IActionResult> GetTotalIncomeByMonth([FromQuery] string email, [FromQuery] int year, [FromQuery] int month)
+        {
+            var result = await _analyticsRepository.GetTotalIncomeByMonthAsync(email, year, month);
+            return Ok(result);
+        }
+
+        [HttpGet("total-expense-this-year")]
+        public async Task<IActionResult> GetTotalExpenseThisYear([FromQuery] string email, [FromQuery] int year)
+        {
+            var result = await _analyticsRepository.GetTotalExpenseThisYearAsync(email, year);
+            return Ok(result);
+        }
+
+        [HttpGet("total-income-this-year")]
+        public async Task<IActionResult> GetTotalIncomeThisYear([FromQuery] string email, [FromQuery] int year)
+        {
+            var result = await _analyticsRepository.GetTotalIncomeThisYearAsync(email, year);
+            return Ok(result);
+        }
+
+        [HttpGet("total-expense-by-category-today")]
+        public async Task<IActionResult> GetTotalExpenseByCategoryToday([FromQuery] string email, [FromQuery] int date, [FromQuery] int month, [FromQuery] int year)
+        {
+            var result = await _analyticsRepository.GetTotalExpenseByCategoryTodayAsync(email, date, month, year);
+            return Ok(result);
+        }
+
+        [HttpGet("total-expense-by-category-this-week")]
+        public async Task<IActionResult> GetTotalExpenseByCategoryThisWeek([FromQuery] string email, [FromQuery] int date, [FromQuery] int month, [FromQuery] int year)
+        {
+            var result = await _analyticsRepository.GetTotalExpenseByCategoryThisWeekAsync(email, date, month, year);
+            return Ok(result);
+        }
+
+        [HttpGet("total-expense-by-category-this-month")]
+        public async Task<IActionResult> GetTotalExpenseByCategoryThisMonth([FromQuery] string email, [FromQuery] int month, [FromQuery] int year)
+        {
+            var result = await _analyticsRepository.GetTotalExpenseByCategoryThisMonthAsync(email, month, year);
+            return Ok(result);
+        }
+
+        [HttpGet("total-expense-by-category-this-year")]
+        public async Task<IActionResult> GetTotalExpenseByCategoryThisYear([FromQuery] string email, [FromQuery] int year)
+        {
+            var result = await _analyticsRepository.GetTotalExpenseByCategoryThisYearAsync(email, year);
             return Ok(result);
         }
     }
--- backend/Repository/Interfaces/IAnalyticsRepository.cs
+++ backend/Repository/Interfaces/IAnalyticsRepository.cs
@@ -1,21 +1,22 @@
 ﻿using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ExpenseTracker.Repository.Interfaces
 {
     public interface IAnalyticsRepository
     {
-        Task<float> GetTotalExpenseTodayAsync(string email);
-        Task<float> GetTotalIncomeTodayAsync(string email);
-        Task<float> GetTotalExpenseThisWeekAsync(string email);
-        Task<float> GetTotalIncomeThisWeekAsync(string email);
+        Task<float> GetTotalExpenseTodayAsync(string email,int date, int month, int year);
+        Task<float> GetTotalIncomeTodayAsync(string email, int date, int month, int year);
+        Task<float> GetTotalExpenseThisWeekAsync(string email,int date,int month,int year);
+        Task<float> GetTotalIncomeThisWeekAsync(string email, int date,int month,int year);
         Task<float> GetTotalExpenseByMonthAsync(string email, int year, int month);
         Task<float> GetTotalIncomeByMonthAsync(string email, int year, int month);
-        Task<float> GetTotalExpenseThisYearAsync(string email);
-        Task<float> GetTotalIncomeThisYearAsync(string email);
+        Task<float> GetTotalExpenseThisYearAsync(string email,int year);
+        Task<float> GetTotalIncomeThisYearAsync(string email, int year);
 
-        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryTodayAsync(string email);
-        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryThisWeekAsync(string email);
-        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryThisMonthAsync(string email);
-        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryThisYearAsync(string email);
+        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryTodayAsync(string email,int date, int month, int year);
+        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryThisWeekAsync(string email, int date, int month, int year);
+        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryThisMonthAsync(string email, int month, int year);
+        Task<IEnumerable<KeyValuePair<string, float>>> GetTotalExpenseByCategoryThisYearAsync(string email,int year);
     }
 }
