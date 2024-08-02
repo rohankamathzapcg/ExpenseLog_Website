60c708edcd6ec18e772b75ab5b14919382099ef7
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-08-02 15:05:23 +0530

    Solved

--- frontend/src/Components/EditAccount.jsx
+++ frontend/src/Components/EditAccount.jsx
@@ -107,7 +107,7 @@ function EditAccount(props) {
 
     return (
         <>
-            <div className="modal fade" id="editAccounts" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal fade" id="editAccounts" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
